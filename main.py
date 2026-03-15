b57d2e229b17cac7bb32109fd16bf5407f5b4d66
Mohmad Saidov <moxmad_saidov@mail.ru>
2026-03-15 13:10:16 +0300

    removing development code

--- main.py
+++ main.py
@@ -6,33 +6,6 @@ app = FastAPI()
 
 app.include_router(price_router)
 
-
-@app.get("/", response_model=dict)
-async def root() -> dict:
-    return {"Message": "Hello world!"}
-
-
-@app.post("/recreate_tables")
-async def recreate_tables():
-    from core.setup import Base, async_engine
-
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
-    return {"Message": "All tables were recreated"}
-
-
-@app.post("/drop_tables")
-async def drop_tables():
-    from core.setup import Base, async_engine
-
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-
-    return {"Message": "All tables were dropped"}
-
-
 if __name__ == "__main__":
     import uvicorn
 
